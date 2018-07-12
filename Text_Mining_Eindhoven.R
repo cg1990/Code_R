19b1180e0ddf1a4bbd0316aad490213fe5a9a12e
Christoph <christoph.gutsche@tu-berlin.de>
2018-07-12 16:06:20 +0200

    Integrate count

--- Text_Mining_Eindhoven.R
+++ Text_Mining_Eindhoven.R
@@ -1,16 +1,27 @@
-getwd()
+library(dplyr)
+library(tidytext)
 
 tilburgText <- paste(readLines("Memo_Tilburg_2.txt"), collapse = " ")
-tilburgText
+  tilburgText
+  
 tilburgText <- gsub(pattern="\\W", replace=" ", tilburgText)
-tilburgText
+  tilburgText
+  
 tilburgText2 <- gsub(pattern="\\d", replace=" ", tilburgText)
-tilburgText2
+  tilburgText2
 tilburgText2 <- tolower(tilburgText2)
-tilburgText2
-library(tm)
-tilburgText2 <- removeWords(tilburgText2, stopwords())
-tilburgText2
+  tilburgText2
+
+tilburgText2 <- removeWords(tilburgText2, stopwords())%>%
+  mutate(linenumber = row_number())%>%
+  tilburgText2
+  count(words())
+
+tidy_tilburgText2 <- tilburgText2
+  tidy_tilburgText2
+  
+text_df <- data_frame(line = 1:4, tilburgText2 = tilburgText2)
+  text_df
 
 
 
