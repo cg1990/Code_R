bd194bd47e0ffe7b7ed6de7092c3ee8e63c8042e
Christoph <christoph.gutsche@tu-berlin.de>
2018-07-16 16:11:43 +0200

    Integrate Counting

--- Text_Mining_Eindhoven.R
+++ Text_Mining_Eindhoven.R
@@ -11,13 +11,29 @@ text
 
 
 library(dplyr)
-tilburgText <- data_frame(line = 1:6, text = text)
-
+  tilburgText <- data_frame(line = 1:5, text = text)
 tilburgText
   
-  
+
 library(tidytext)
+tidy_books <- tilburgText %>%
+  unnest_tokens(word, text)
 
-tilburgText %>%
-  unnest_tokens(word, text)  
+data(stop_words)
+  tidy_books <- tidy_books %>%
+    anti_join(stop_words)
 
+library(dplyr)
+  tidy_books %>%
+    count(word, sort = TRUE) 
+  tidy_books
+
+  nrc_joy <- get_sentiments("nrc") %>% 
+    filter(sentiment == "joy")
+  
+  tidy_books %>%
+
+    inner_join(nrc_joy) %>%
+    count(word, sort = TRUE)  
+  tidy_books
+  
